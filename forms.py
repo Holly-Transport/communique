0932763bbf59d5a0de929219e4c7d3c90222ce1d
Holly <jeepney.survey@gmail.com>
2021-03-07 20:47:58 -0500

    Updates

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+
+
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    date = StringField("Date (Month DD, YYYY)")
+    # author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+class Registration(FlaskForm):
+    name = StringField("First Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password =PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+class Login(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+class Comments(FlaskForm):
+    text = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField("Comment")
